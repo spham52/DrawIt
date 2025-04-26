0a66e1fc5ddbadf77495112eb753e873eca46021
Steven <pcmrsteven@gmail.com>
2025-04-26 12:56:38 +1000

    Add Spring backend files

---
+++ backend/src/main/java/com/drawit/demo/DrawitApplication.java
@@ -0,0 +1,13 @@
+package com.drawit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/drawit/demo/DrawitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drawit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrawitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
