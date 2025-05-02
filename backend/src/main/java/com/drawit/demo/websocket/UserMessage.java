b334d72ffbda7d1906a26a34e7c03aebba21e4b8
Steven <pcmrsteven@gmail.com>
2025-05-02 17:12:58 +1000

    Create POJO UserMessage for Session Creation in front-end

---
+++ backend/src/main/java/com/drawit/demo/websocket/UserMessage.java
@@ -0,0 +1,31 @@
+package com.drawit.demo.websocket;
+
+public class UserMessage {
+
+    private String username;
+    private String inviteCode;
+
+    public UserMessage(String username, String inviteCode) {
+        this.username = username;
+        this.inviteCode = inviteCode;
+    }
+
+    public UserMessage() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String username) {
+        this.username = username;
+    }
+
+    public String getInviteCode() {
+        return inviteCode;
+    }
+
+    public void setInviteCode(String inviteCode) {
+        this.inviteCode = inviteCode;
+    }
+}
