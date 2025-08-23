9df5a5f5b62e4c0d9a312c6679d1a8d239fd92e6
Steven <pcmrsteven@gmail.com>
2025-08-23 16:44:02 +1000

    add GameServiceImpl - store games in ConcurrentHashMap keyed by GameID - add functions to retrieve and add games - add function to add players game to game

---
+++ backend/src/main/java/com/drawit/demo/service/GameService.java
@@ -0,0 +1,17 @@
+package com.drawit.demo.service;
+
+import com.drawit.demo.model.Game;
+import com.drawit.demo.model.Player;
+
+import java.util.UUID;
+
+public interface GameService {
+
+    Game getGame(UUID gameID);
+
+    void addGame(Game game);
+
+    void addPlayer(Player player, UUID gameID);
+
+    void stopGame(UUID gameID);
+}
---
+++ backend/src/main/java/com/drawit/demo/service/GameServiceImpl.java
@@ -0,0 +1,38 @@
+package com.drawit.demo.service;
+
+import com.drawit.demo.model.Game;
+import com.drawit.demo.model.Player;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class GameServiceImpl implements GameService {
+    private final Map<UUID, Game> games = new ConcurrentHashMap<>();
+
+    public GameServiceImpl() {
+    }
+
+    @Override
+    public Game getGame(UUID gameID) {
+        return games.get(gameID);
+    }
+
+    @Override
+    public void addGame(Game game) {
+        games.put(game.getGameID(), game);
+    }
+
+    @Override
+    public void addPlayer(Player player, UUID gameID) {
+        Game game = getGame(gameID);
+        game.getPlayers().put(player.getId(), player);
+    }
+
+    @Override
+    public void stopGame(UUID gameID) {
+
+    }
+}
