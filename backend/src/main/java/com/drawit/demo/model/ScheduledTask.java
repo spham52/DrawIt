f5b311324d8602cc3cbc903ef7b31c763eb9d2f3
Steven <pcmrsteven@gmail.com>
2025-08-23 16:48:00 +1000

    create ScheduledTask class
    - add ScheduledTask wrapper for managing scheduled game executors

---
+++ backend/src/main/java/com/drawit/demo/model/ScheduledTask.java
@@ -0,0 +1,28 @@
+package com.drawit.demo.model;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+public class ScheduledTask {
+
+    private final ScheduledExecutorService executor;
+    private final ScheduledFuture<?> future;
+
+    public ScheduledTask(ScheduledExecutorService executor, ScheduledFuture<?> future) {
+        this.executor = executor;
+        this.future = future;
+    }
+
+    public void cancel() {
+        future.cancel(false);
+        executor.shutdown();
+    }
+
+    public boolean isCancelled() {
+        return future.isCancelled();
+    }
+
+    public boolean isDone() {
+        return future.isDone();
+    }
+}
