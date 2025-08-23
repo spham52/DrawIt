90041c9d7e7b56cd4ab86f84c6898f41fa2932ac
Steven <pcmrsteven@gmail.com>
2025-08-23 16:28:13 +1000

    Add enums for EventMessageType, used for sending game announcements on websocket channel

---
+++ backend/src/main/java/com/drawit/demo/model/enums/EventMessageType.java
@@ -0,0 +1,12 @@
+package com.drawit.demo.model.enums;
+
+public enum EventMessageType {
+    CORRECT_GUESS,
+    CURRENT_WORD,
+    PLAYER_JOINED,
+    PLAYER_LEFT,
+    ROUND_START,
+    ROUND_END,
+    CORRECT_GUESS_ANNOUNCEMENT,
+    GAME_OVER,
+}
