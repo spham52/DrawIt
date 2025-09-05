59cdd56c3e51e6d758e9f4496736cc04d287a8b3
Steven <pcmrsteven@gmail.com>
2025-09-05 18:43:48 +1000

    Implement Coordinates POJO for tracking coordinates of player drawings

---
+++ backend/src/main/java/com/drawit/demo/dto/Coordinates.java
@@ -0,0 +1,48 @@
+package com.drawit.demo.dto;
+
+// used in websocket communication for tracking coordinates of player's drawing
+public class Coordinates {
+    int x;
+    int y;
+    int prevX;
+    int prevY;
+
+    public Coordinates(int x, int y, int prevX, int prevY) {
+        this.x = x;
+        this.y = y;
+        this.prevX = prevX;
+        this.prevY = prevY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getPrevX() {
+        return prevX;
+    }
+
+    public void setPrevX(int prevX) {
+        this.prevX = prevX;
+    }
+
+    public int getPrevY() {
+        return prevY;
+    }
+
+    public void setPrevY(int prevY) {
+        this.prevY = prevY;
+    }
+}
